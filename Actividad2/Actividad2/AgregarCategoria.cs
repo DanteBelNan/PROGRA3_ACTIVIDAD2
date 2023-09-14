ac203953d7a390be9fb696bbd6664abb4d8a1979
Robielys <116286724+Robielys@users.noreply.github.com>
2023-09-14 12:49:00 -0300

    Agregar Categoría lista
    se encuentra funcionan, falta validacion

--- Actividad2/Actividad2/AgregarCategoria.cs
+++ Actividad2/Actividad2/AgregarCategoria.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dominio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using negocio;
 
 namespace Actividad2
 {
@@ -15,6 +17,39 @@ namespace Actividad2
         public AgregarCategoria()
         {
             InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAgrega_Click(object sender, EventArgs e)
+        {
+            Categoria nueva= new Categoria();
+            CategoriaDB categoriaDB = new CategoriaDB();
+
+           try
+           {
+            //nueva.Id = int.Parse(txtIdCat.Text);
+            nueva.Descripcion = txtDescCat.Text;
+                categoriaDB.agregar(nueva);
+                MessageBox.Show("Agregado exitosamente");
+                Close();
+
+
+           }
+            catch (Exception ex)
+           {
+                MessageBox.Show(ex.ToString());
+           }
+
+              
+        }
+
+        private void AgregarCategoria_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
